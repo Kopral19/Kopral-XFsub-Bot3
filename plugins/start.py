885af02e25cb33933a58218df3d7bffce027b1e4
OURA? XINN? <99221379+Oura-Ubot@users.noreply.github.com>
2022-04-22 00:28:58 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -115,7 +115,6 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink1),
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink2),
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
         ]
     ]
     try:
